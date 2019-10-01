b06eae282932798cc1565d4572d702d35c2f11cc
Kyle Aquino <aquino.kylekay@gmail.com>
2019-10-01 19:45:12 -0400

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+//Global Variables
+alert("test");
+
+//Crystal Variables
+var crystal = {
+    blue:{
+        name: "blue",
+        value: 0
+    },
+    
+    red:{
+        name: "red",
+        value:0
+    }
+
+    purple:{
+        name: "purple"
+        value: 0
+    }
+
+    yellow: {
+        name: "yellow"
+        value: 0
+    }
+};
+
+//Scores (current and target)
+var currentScore = 0;
+var targetScore  = 0;
+
+//Wins and Losses
+var winCount     = 0;
+var lossCount    = 0;
+
+
+
+//Functions
+
+
+
+
+//Main Process
+$("#blue").click(function() {
+    
+});
+
+
